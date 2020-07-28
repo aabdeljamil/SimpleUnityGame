07e7ebc1ff42826f257fe3fd69b01d2c228a2080
aabdeljamil <43014818+aabdeljamil@users.noreply.github.com>
2020-07-28 16:13:13 -0500

    Level 1 completed

--- Simple3D/Assets/playerMovement.cs
+++ Simple3D/Assets/playerMovement.cs
@@ -6,12 +6,12 @@ public class playerMovement : MonoBehaviour
 {
     private float movementInputDirection;
     private Rigidbody rb;
+    public float horizontalSpeed;
     public float movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(0, 0, -1000);
     }
 
     // Update is called once per frame
@@ -27,6 +27,6 @@ public class playerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(movementSpeed * -1 * movementInputDirection, rb.velocity.y, rb.velocity.z);
+        rb.velocity = new Vector3(horizontalSpeed * -1 * movementInputDirection, rb.velocity.y, -movementSpeed);
     }
 }
