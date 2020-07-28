03e46fee253df9e1d069390a3a777ac9e5577e96
unknown <billwang56@gmail.com>
2020-07-28 16:21:53 -0500

    asd

---
+++ Simple3D/Assets/Script/LoadNextLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public float transitionTime = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        LoadnxtLevel();
+    }
+
+    public void LoadnxtLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
