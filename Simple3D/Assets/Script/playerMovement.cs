9366590a87e94687d8d8df8887d826b2deb68b46
unknown <billwang56@gmail.com>
2020-07-29 15:29:27 -0500

    asd

--- Simple3D/Assets/Script/playerMovement.cs
+++ Simple3D/Assets/Script/playerMovement.cs
@@ -8,6 +8,11 @@ public class playerMovement : MonoBehaviour
     private Rigidbody rb;
     public float horizontalSpeed;
     public float movementSpeed;
+    public float gravityScale = 1f;
+    public float jumpSpeed = 10;
+    public bool isGrounded;
+    public float jumpPressed;
+    public float jumpPressedTime = .2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +33,24 @@ public class playerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(horizontalSpeed * -1 * movementInputDirection, rb.velocity.y, -movementSpeed);
+        jumpPressed -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = jumpPressedTime;
+        }
+
+        if (jumpPressed > 0 && isGrounded)
+        {
+            jumpPressed = 0;
+            rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+       rb.velocity -= new Vector3(0,Physics.gravity.y * gravityScale * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
     }
 }
