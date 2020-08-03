8ae63e41eff36f93f1ff899cf3cdb4efb80125b4
aabdeljamil <43014818+aabdeljamil@users.noreply.github.com>
2020-08-02 19:45:59 -0500

    renamed folder

---
+++ Simple3D/Assets/Scripts/Respawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform player;
+    public Transform respawnPoint;
+    public static bool isDead;
+    public DeathText text;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            StartCoroutine(DieScreen());
+    }
+
+    IEnumerator DieScreen()
+    {
+        player.gameObject.SetActive(false);
+        text.TurnOn();
+        yield return new WaitForSeconds(1.5f);
+        text.TurnOff();
+        player.transform.position = respawnPoint.transform.position;
+        player.gameObject.SetActive(true);
+    }
+}
