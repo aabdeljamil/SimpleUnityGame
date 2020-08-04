a9b9c502f26aacbcf30f9e4c229fc6a515cd7d1d
unknown <billwang56@gmail.com>
2020-08-04 14:18:19 -0500

    added start menu

--- Simple3D/Assets/Script/playerMovement.cs
+++ Simple3D/Assets/Script/playerMovement.cs
@@ -8,7 +8,7 @@ public class playerMovement : MonoBehaviour
     private float movementInputDirection;
     public static Rigidbody rb;
     public float horizontalSpeed;
-    public float movementSpeed;
+    public static float movementSpeed = 35;
     public float gravityScale = -3f;
     public float jumpSpeed = 20f;
     public float jumpShortSpeed = 10f;
@@ -27,7 +27,7 @@ public class playerMovement : MonoBehaviour
     void Update()
     {
         OnMove();
-        if (SceneManager.GetActiveScene().buildIndex != 0)
+        if (SceneManager.GetActiveScene().buildIndex != 1)
         {
             if (Input.GetButtonDown("Jump"))
             {
@@ -48,18 +48,16 @@ public class playerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (movementSpeed > 35)
+        {
+            movementSpeed -= 1;
+        }
+
         rb.velocity = new Vector3(horizontalSpeed * -1 * movementInputDirection, rb.velocity.y, -movementSpeed);
         jumpPressed -= Time.deltaTime;
 
         if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            if (jumpPressed > 0 && isGrounded)
-            {
-                jumpPressed = 0;
-                rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
-                isGrounded = false;
-            }
-
             if (jumpcancel)
             {
                 if (rb.velocity.y > jumpShortSpeed)
@@ -67,9 +65,19 @@ public class playerMovement : MonoBehaviour
                     rb.velocity = new Vector3(rb.velocity.x, jumpShortSpeed, rb.velocity.z);
                 }
             }
+            if (jumpPressed > 0 && isGrounded)
+            {
+                jumpPressed = 0;
+                rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+                isGrounded = false;
+            }
+
         }
 
        rb.velocity -= new Vector3(0,Physics.gravity.y * gravityScale * Time.deltaTime, 0);
+        if (rb.velocity.z > -movementSpeed)
+            rb.velocity += new Vector3(0, 0, 1);
+
     }
 
     private void OnCollisionEnter(Collision collision)
