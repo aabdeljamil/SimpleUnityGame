e23bb78f0f48d72f0c0b8fb692196f475b224dc2
unknown <billwang56@gmail.com>
2020-08-09 12:44:01 -0500

    changes to lvl 7

---
+++ Simple3D/Assets/Script/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+            playerMovement.rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
